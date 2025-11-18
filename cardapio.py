4cb6410b6fbf16006743da7a64c029963f0625f4
caiospaling <caiospaling@gmail.com>
2025-11-18 23:37:10 +0000

    Enviando arquivo

---
+++ cardapio.py
@@ -0,0 +1,12 @@
+def buscar_cardapio():
+    itens = [
+        {
+            "id": 1,
+            "nome": "Red Velvet",
+            "descricao": "Um delicioso bolo que mistura o vermelho e branco!",
+            "preco": 162.90,
+            "foto": ""
+
+        }
+    ]
+    return itens
