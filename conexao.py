069b9a90148eb93800a583adfd5f62d3eab91541
CaioSpaling <caiospaling@gmail.com>
2025-12-03 20:21:07 -0300

    Update env

--- conexao.py
+++ conexao.py
@@ -1,11 +1,13 @@
 import psycopg2
+from dotenv import load_dotenv
 
 def get_conexao():
-    conn =  psycopg2.connect(
-        dbname = 'postgres',
-        user = 'postgres.ohzwnzzmnhvulvfxbgfa',
-        password = 'Aquarela13#@!',
-        host = 'aws-1-us-east-2.pooler.supabase.com',
-        port = '6543'
+    conn = psycopg2.connect(
+        dbname=os.getenv("DB_DATABASE"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        host=os.getenv("DB_HOST"),
+        port=os.getenv("DB_PORT"),
+        sslmode="require"
     )
     return conn
